9d4f8c2ac46b0578cdbd06a67c8526b84f067c41
Aleem pasha <aleempasha1410@gmail.com>
2024-05-27 12:21:51 +0530

    Business logic - Component logic implementation

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { TodoService } from '../todo.service';
+import { Todo } from '../todo.model';
 
 @Component({
   selector: 'app-todo-list',
@@ -7,9 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodoListComponent implements OnInit {
 
-  constructor() { }
+  //creating a dictionary to save data coming from service
+  todos:Todo[] = [];
 
+ //instaciating todoService in the constructor
+  constructor(private todoService:TodoService) { }
+
+
+  //when ever this component is called it automatically calls the getTodo method
   ngOnInit(): void {
+    this.todoService.getTodo().subscribe(todos=>this.todos = todos);
   }
+
+  
+  toggleCompletion(todo:Todo):void{
+    this.todoService.toggleTodoCOmpletion(todo);
+  }
+
+
 
 }
